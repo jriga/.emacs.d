d73ccaa91b9ac973e41995922e9959c85c0fd4be
Jerome Riga <jriga@zemis.co.uk>
2015-09-18 18:05:51 +0100

    fix jslint loading error

--- customisations/my-js.el
+++ customisations/my-js.el
@@ -12,11 +12,14 @@
   (paredit-mode 1))
 
 
-;; JSHint
-;; before run cmd `$npm install -g jshint`
-(require 'flymake-jshint)
-(add-hook 'js-mode-hook 'flymake-jslint-load)
+(add-hook 'js-mode-hook 'my-paredit-nonlisp)
 (add-hook 'js-mode-hook 'electric-pair-mode)
+
+;; JSHint
+;; before run cmd `$npm install -g jshint jslint`
+(require 'flymake-jslint)
+(add-hook 'js-mode-hook 'flymake-jslint-load)
+
 
 ;; JS intdent 2 spaces for TAB
 (setq js-indent-level 2)
